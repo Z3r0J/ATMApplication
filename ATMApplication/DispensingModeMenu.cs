85742483d497e9e3821530ed7afbc9be9baaad66
Jean Carlos Reyes Encarnacion <yacalo45@hotmail.es>
2021-10-19 22:45:03 -0400

    Edit Menus

--- ATMApplication/DispensingModeMenu.cs
+++ ATMApplication/DispensingModeMenu.cs
@@ -6,21 +6,29 @@ namespace ATMApplication
     {
         public void DispensingMenu()
         {
-            Console.WriteLine(" 1 - 200 & 100\n" +
-                "2 - 100 & 500\n" +
-                "3 - Efficient Mode (Defect Mode) 100 , 200 , 500 , 1000");
-            int option = Convert.ToInt32(Console.ReadLine());
-
-            switch (option)
+            try
             {
-                case (int)ModeDispensing.TwoHundredandOneThousand:
-                    break;
-                case (int)ModeDispensing.OneHundredandFiveHundred:
-                    break;
-                case (int)ModeDispensing.EfficientMode:
-                    break;
-                default:
-                    break;
+                Console.WriteLine(" 1 - 200 & 100\n" +
+               "2 - 100 & 500\n" +
+               "3 - Efficient Mode (Defect Mode) 100 , 200 , 500 , 1000");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case (int)ModeDispensing.TwoHundredandOneThousand:
+                        break;
+                    case (int)ModeDispensing.OneHundredandFiveHundred:
+                        break;
+                    case (int)ModeDispensing.EfficientMode:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
             }
 
         }
--- ATMApplication/PrincipalMenu.cs
+++ ATMApplication/PrincipalMenu.cs
@@ -10,22 +10,29 @@ namespace ATMApplication
         DispensingModeMenu modeMenu = new DispensingModeMenu();
         public void PrintMenu()
         {
-            Console.WriteLine("1 - Dispense Mode\n"+"2 - Withdraw Money\n" +"3 - Exit");
-            Console.Write("> ");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
+            try
             {
-                case (int)InitialMenu.Mode:
-                    modeMenu.DispensingMenu();
-                    break;
-                case (int)InitialMenu.Withdraw:
-                    break;
-                case (int)InitialMenu.Exit:
-                    break;
-                default:
-                    break;
-            }
+                Console.WriteLine("1 - Dispense Mode\n" + "2 - Withdraw Money\n" + "3 - Exit");
+                Console.Write("> ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case (int)InitialMenu.Mode:
+                        modeMenu.DispensingMenu();
+                        break;
+                    case (int)InitialMenu.Withdraw:
+                        break;
+                    case (int)InitialMenu.Exit:
+                        break;
+                    default:
+                        break;
+                }
 
+            }
+            catch (Exception ex)
+            {
+
+            }
 
         }
     }
